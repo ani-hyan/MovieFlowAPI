beb14a7095549e84937d9862b25dc328af1b26be
ahakobyan <ahakobyan@griddynamics.com>
2024-02-25 17:06:28 +0400

    TOOL-1 Create spring boot project

---
+++ src/main/java/com/example/movieflowapi/MovieFlowApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.movieflowapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieFlowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieFlowApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/movieflowapi/MovieFlowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movieflowapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieFlowApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
