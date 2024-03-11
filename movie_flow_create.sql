f1dfbc12c79ea50899458b629b0b8a5ee9f22ae9
ahakobyan <ahakobyan@griddynamics.com>
2024-03-11 13:42:31 +0400

    Update foreign key settings

--- movie_flow_create.sql
+++ movie_flow_create.sql
@@ -3,7 +3,7 @@ SET search_path = movie_flow;
 
 CREATE TABLE IF NOT EXISTS movie (
     id SERIAL PRIMARY KEY,
-    release_date TIMESTAMP NOT NULL,
+    release_date DATE NOT NULL,
     description TEXT,
     duration_mins INTEGER,
     language TEXT NOT NULL
@@ -37,41 +37,32 @@ CREATE TABLE IF NOT EXISTS def_provider_company (
 
 CREATE TABLE IF NOT EXISTS rating (
 	id SERIAL PRIMARY KEY,
+    rating_source TEXT NOT NULL,
     rate DOUBLE PRECISION NOT NULL,
     max_rate DOUBLE PRECISION NOT NULL,
-    movie_id INT NOT NULL,
-    FOREIGN KEY (movie_id) REFERENCES movie(id) ON UPDATE NO ACTION ON DELETE NO ACTION NOT VALID
+    movie_id INT NOT NULL REFERENCES movie(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS n2n_movie_to_actor (
-    movie_id INT NOT NULL,
-    actor_id INT NOT NULL,
-    PRIMARY KEY (movie_id, actor_id),
-    FOREIGN KEY (movie_id) REFERENCES movie(id) ON UPDATE NO ACTION ON DELETE NO ACTION NOT VALID,
-    FOREIGN KEY (actor_id) REFERENCES actor(id) ON UPDATE NO ACTION ON DELETE NO ACTION NOT VALID
+    movie_id INT NOT NULL REFERENCES movie(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    actor_id INT NOT NULL REFERENCES actor(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS n2n_movie_to_external_platform (
-    movie_id INT NOT NULL,
-    external_platform_id INT NOT NULL,
+    movie_id INT NOT NULL REFERENCES movie(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    external_platform_id INT NOT NULL REFERENCES actor(id) ON DELETE CASCADE ON UPDATE CASCADE,
     movie_url TEXT NOT NULL,
-    PRIMARY KEY (movie_id, external_platform_id),
-    FOREIGN KEY (movie_id) REFERENCES movie(id) ON UPDATE NO ACTION ON DELETE NO ACTION NOT VALID,
-    FOREIGN KEY (external_platform_id) REFERENCES def_external_platform(id) ON UPDATE NO ACTION ON DELETE NO ACTION NOT VALID
+    PRIMARY KEY (movie_id, external_platform_id)
 );
 
 CREATE TABLE IF NOT EXISTS n2n_movie_to_genre (
-    movie_id INT NOT NULL,
-    genre_id INT NOT NULL,
-    PRIMARY KEY (movie_id, genre_id),
-    FOREIGN KEY (movie_id) REFERENCES movie(id) ON UPDATE NO ACTION ON DELETE NO ACTION NOT VALID,
-    FOREIGN KEY (genre_id) REFERENCES def_genre(id) ON UPDATE NO ACTION ON DELETE NO ACTION NOT VALID
+    movie_id INT NOT NULL REFERENCES movie(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    genre_id INT NOT NULL REFERENCES actor(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    PRIMARY KEY (movie_id, genre_id)
 );
 
 CREATE TABLE IF NOT EXISTS n2n_movie_to_provider_company (
-    movie_id INT NOT NULL,
-    provider_company_id INT NOT NULL,
-    PRIMARY KEY (movie_id, provider_company_id),
-    FOREIGN KEY (movie_id) REFERENCES movie(id) ON UPDATE NO ACTION ON DELETE NO ACTION NOT VALID,
-    FOREIGN KEY (provider_company_id) REFERENCES def_provider_company(id) ON UPDATE NO ACTION ON DELETE NO ACTION NOT VALID
+    movie_id INT NOT NULL REFERENCES movie(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    provider_company_id INT NOT NULL REFERENCES actor(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    PRIMARY KEY (movie_id, provider_company_id)
 );
